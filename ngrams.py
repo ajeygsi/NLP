1c66ce9bb05c5c2d887ba1f628012f66bac0e57a
ajey <ajeygsi2@gmail.com>
2011-09-21 15:34:59 +0530

    Finished phrase level.

--- ngrams.py
+++ ngrams.py
@@ -1,42 +1,42 @@
+# Phrase level spell checker
+
 #!/usr/bin/python
 import time
 import sys
 import nltk, time, re
-from nltk.corpus import wordnet as wn
 from nltk.corpus import brown
 
-N = 2
+sents1 = brown.sents()
+sents = []
+for sent in sents1:
+    sents.append([word.lower() for word in sent])
 
-words = []
-for word in brown.words():
-    words.append(word.lower())
-
-ngrams = nltk.ngrams(words,N)
-FreqDist = nltk.FreqDist(ngrams)
+bigrams = []
+for sent in sents:
+    bigrams.extend(nltk.bigrams(sent[:-1]))   # End of sentence marker '.' does not have much significance in phrases
+bigramDist = nltk.FreqDist(bigrams)
 
 # Constructing the reverse table. (fig 6.10 jurafsky martin)
 REVERSE_TABLE = {}
-for key in FreqDist:
-    r_key = FreqDist[key]
+for key in bigramDist:
+    r_key = bigramDist[key]
     if REVERSE_TABLE.has_key(r_key):
         REVERSE_TABLE[r_key] += 1;
     else:
         REVERSE_TABLE[r_key] = 1;
 
 # Calculating the value of N0.
-VOCAB_SIZE = 49815              # computed specifically for the brown corpus.
-N0  = VOCAB_SIZE*VOCAB_SIZE - len(FreqDist)
+VOCAB_SIZE = 49814              # computed specifically for the brown corpus, '.' is not counted
+N0  = VOCAB_SIZE*VOCAB_SIZE - len(bigramDist)
 REVERSE_TABLE[0] = N0
 
 
 # The following logic works only for the bigram case.
-def correctPhrase(phrase, k):
-    phrase = phrase.lower()
-    pwords = phrase.split(' ')
-
+# phrase is a list of words
+def getProbabilityOfPhrase(phrase, k, p3):
     prob = 1.0
-    for i in xrange(1, len(pwords)):
-        c = FreqDist[ (pwords[i-1], pwords[i]) ]
+    for i in xrange(1, len(phrase)):
+        c = bigramDist[ (phrase[i-1], phrase[i]) ]
         # Good turing adjusted count.
         if c == 0:
             c_star = float(REVERSE_TABLE[1]) / float(REVERSE_TABLE[0])
@@ -46,7 +46,7 @@ def correctPhrase(phrase, k):
             temp = float(k + 1) * float(REVERSE_TABLE[k+1]) / float(REVERSE_TABLE[1])
             c_star = (float(c+1) * float(REVERSE_TABLE[c+1]) / float(REVERSE_TABLE[c]) - float(c) * temp )/ (1 - temp)
 
-        print i, c_star
+ #       print i, c_star
         prob *= c_star
 
     return prob
--- test_checker.py
+++ test_checker.py
@@ -1,10 +1,22 @@
 import spell_checker            # Assuming it is in the same directory.
 from xml.etree import ElementTree as ET
 import re
+import utilities
+#import pos_ngrams
+import ngrams
 
 
-MAX_UNIGRAM_SUGGESTIONS = 10
+MAX_UNIGRAM_SUGGESTIONS = 6
 MAX_SUGGESTIONS = 5
+
+regex_punct = re.compile('[,|:|\'|\"|.|?|!]')
+def isPunctuation(str):
+    if(( len(str) == 1) and (regex_punct.match(str))):
+        return True
+    return False
+
+def getSecond(tuple):
+    return tuple[1]
 
 # function to read the input file which is in xml format
 # and write the output to the output file.
@@ -17,6 +29,9 @@ def test(input_file_name,output_file_name):
     
     # regex for single word.
     regex_single = re.compile('\w+')
+
+    # regex for phrase.
+    regex_phrase = re.compile('\w+|[,|:|(|)|\'|\"]')
     
     # creating the regex obj.
     regex = re.compile('\w+|[.|,|:|(|)|\'|\"]')
@@ -24,9 +39,11 @@ def test(input_file_name,output_file_name):
 
     
     for subelement in element:
-        outfile.write(subelement.attrib['id'])
+
 
         if(subelement.attrib['id'][0] == '1'): # testcase1.xml
+            outfile.write(subelement.attrib['id'])
+
             single_word = regex_single.findall(subelement.text)
             
             suggestions = spell_checker.correct(single_word[0])
@@ -45,25 +62,64 @@ def test(input_file_name,output_file_name):
                     outfile.write("\n")
                     break
 
+        elif( subelement.attrib['id'][0] == '2'): # testcase2.xml
+            words_in_phrase = regex_phrase.findall( subelement.text)  
+
+            # identifying the incorrect words in the phrase.
+            i = 0;
+            for i in xrange(0,len(words_in_phrase)):
                 
-        else:
-            sentences = subelement.text.split('.')             # dot is the sentence separator.
+                # if it is a punctuations then we should not query the dictionary.
+                if( isPunctuation(words_in_phrase[i])):
+                    continue
+
+                suggestions = spell_checker.correct(words_in_phrase[i])
+                if( len(suggestions) == 1): # correct word.
+                    continue
+
+                outfile.write(subelement.attrib['id'])
+                incorrect_word = words_in_phrase[i]
+                outfile.write(", "+incorrect_word)
+
+                temp_list = []
+                for each_suggestion in suggestions[:MAX_UNIGRAM_SUGGESTIONS] :
+                    words_in_phrase[i] = each_suggestion[0]
+                    temp_list.append( ( each_suggestion[0], ngrams.getProbabilityOfPhrase(words_in_phrase,100,False)))
+
+
+                temp_list.sort( key = getSecond, reverse=True)
+
+                i = 1
+                # printing the suggestions.
+                for each_temp in temp_list:
+                    outfile.write(", ")
+                    outfile.write(each_temp[0])
+
+                    i += 1
+                    if( i > MAX_SUGGESTIONS):
+                        outfile.write("\n")
+                        break
+        # elif( (subelement.attrib['id'][0] == '3') or (subelement.attrib['id'][0] == '4') ): # testcases3 or 4.xml
             
-            for each_sentence in sentences:
-                words_in_sentence = regex.findall(each_sentence) # a list is returned.
+                    
+        # elif( subelement.attrib['id'][0] == '2'): # testcase2.xml
+        #     sentences = subelement.text.split('.')             # dot is the sentence separator.
+            
+        #     for each_sentence in sentences:
+        #         words_in_sentence = regex.findall(each_sentence) # a list is returned.
                 
-                lolists = []
-                for each_word in words_in_sentence:
+        #         lolists = []
+        #         for each_word in words_in_sentence:
                     
-                    # Check whether the word is present in the dictionary.
-                    suggestions = spell_checker.correct(each_word)
-                    new_suggestions = []
-                    for each_suggestion in suggestions:
-                        new_suggestions.append(each_suggestion[0])
+        #             # Check whether the word is present in the dictionary.
+        #             suggestions = spell_checker.correct(each_word)
+        #             new_suggestions = []
+        #             for each_suggestion in suggestions:
+        #                 new_suggestions.append(each_suggestion[0])
                         
-                    lolists.append(new_suggestions[:MAX_UNIGRAM_SUGGESTIONS])
+        #             lolists.append(new_suggestions[:MAX_UNIGRAM_SUGGESTIONS])
                     
-                print lolists
+        #         print lolists
 
                 
                         
